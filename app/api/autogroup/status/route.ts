d0502688d407d63deaa18ed2804c72a7525c2ed5
psychicZebra <nicolelung20@gmail.com>
2026-09-16 18:55:14 -0400

    Fix production AUTOGROUP API module path

--- app/api/autogroup/status/route.ts
+++ app/api/autogroup/status/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { buildAutogroupSnapshot } from "../../../../../src/autogroup247.js";
+import { buildAutogroupSnapshot } from "../../../../src/autogroup247.js";
 
 export const dynamic = "force-dynamic";
 
